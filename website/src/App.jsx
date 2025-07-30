07022e6819c4bd641fef434b020dfc350a451eff
Joe Fearnley <joe.fearnley@gmail.com>
2025-07-30 19:07:07 -0400

    Setting up vite and initial UI stuff

---
+++ website/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="container mx-auto">
+                <div className="mx-auto text-center py-12">
+                    <h1 className="text-5xl font-bold">Mega Man Dooms Day</h1>
+                </div>
+            </div>
+            <div className="container mx-auto">
+                <div className="grid grid-cols-3 gap-3">
+                    <h1 className="text-5xl font-bold">Mega Man Dooms Day</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
